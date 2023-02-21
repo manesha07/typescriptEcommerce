edfceb81e2ec3ed9a6e48910444f3916801f39cf
Sujata Gaihre <40952778+nirjana@users.noreply.github.com>
2023-02-21 13:50:53 +0545

    Update knexfile.js

--- backend/dist/knexfile.js
+++ backend/dist/knexfile.js
@@ -5,43 +5,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connection = void 0;
 const knex_1 = __importDefault(require("knex"));
-const configuration = {
-    client: "pg",
-    connection: {
-        host: "localhost",
-        port: 5432,
-        user: "postgres",
-        password: "postgres",
-        database: "typescriptecdb",
-    },
-    migrations: {
-        tableName: "migrations",
-    },
-    seeds: {
-        directory: "./seeds",
-        stub: "./stub/seed.stub",
-    },
-};
+// const configuration = {
+//     client: "pg",
+//     connection: {
+//         host: "localhost",
+//         port: 5432,
+//         user: "postgres",
+//         password: "postgres",
+//         database: "typescriptecdb",
+//     },
+//     migrations: {
+//         tableName: "migrations",
+//     },
+//     seeds: {
+//         directory: "./seeds",
+//         stub: "./stub/seed.stub",
+//     },
+// };
 // ELEPHANT SUJATA
 // ELEPHANT
 // import Knex from 'knex';
-// const configuration = {
-//   client: "pg",
-//   connection: {
-//     host: "satao.db.elephantsql.com",
-//     port: 5432,
-//     user: "ixfkndjf",
-//     password: "pai8KiXmaa-ObCf32QNLfPddm1MpoOJ0",
-//     database: "ixfkndjf",
-//   },
-//   migrations: {
-//     tableName: "migrations",
-//   },
-//   seeds: {
-//     directory: "./seeds",
-//     stub: "./stub/seed.stub",
-//   },
-// };
+const configuration = {
+  client: "pg",
+  connection: {
+    host: "satao.db.elephantsql.com",
+    port: 5432,
+    user: "ixfkndjf",
+    password: "pai8KiXmaa-ObCf32QNLfPddm1MpoOJ0",
+    database: "ixfkndjf",
+  },
+  migrations: {
+    tableName: "migrations",
+  },
+  seeds: {
+    directory: "./seeds",
+    stub: "./stub/seed.stub",
+  },
+};
 exports.connection = (0, knex_1.default)(configuration);
 exports.default = configuration;
 // //ELEPHANT
